765f3df4b2b2f2085d6d9f1bd5a098e04635adf4
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-03 18:40:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ require 'pry'
 class CashRegister
   attr_accessor :total, :discount
   
-  def initialize(total = 0, discount = 20)
+  def initialize(total, discount = 20)
     @total = total
     @discount = discount
   end 
